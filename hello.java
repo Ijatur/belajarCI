bb1d69d86153218ca701b7cba19fbc9127df6d6b
Ijat <turamadhanizza@gmail.com>
2020-01-06 11:45:07 +0700

    ganti

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello{
 	public static void main(String[] args) {
-		System.out.println("Hello mamank ujang geblek");
+		System.out.println("Hello mamank anda tau sholat mamank sholat yuk!");
 	}
 }
